68e63c8d2e4b0e6ebe5fc387481a5cde6d92c00f
Роман <romamorozevich@yandex.ru>
2022-07-04 13:33:33 +0300

    commit

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
-import requests as requests
+
+
+import requests
+
+from pprint import pprint
 
 
 
-URL = 'https://akabab.github.io/superhero-api/api'
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
 
-superheroes_ids = {'Hulk': '332', 'Thanos': '655', 'Capitan America': '149'}
-
-superheroes_stats = []
-
-for id in superheroes_ids:
-    response = requests.get(URL + '/powerstats/' + superheroes_ids[id] + '.json')
-    superhero_stats = {id: response.json()}
-    superheroes_stats.append(superhero_stats)
-
-superheroes_int = []
-
-for superhero in superheroes_stats:
-    for stat in superhero:
-        intelligence = superhero[stat]['intelligence']
-        superhero_int = {stat: intelligence}
-        superheroes_int.append(superhero_int)
+    def get_headers(self):
+        return {
+            'Content-Type' : 'application/json',
+            'Authorization' : 'OAuth {}'.format(self.token)
+        }
+    def upload(self, file_path):
+        URL = 'https://cloud-api.yandex.net:443/v1/disk/resources/upload'
+        headers = self.get_headers()
+        params = {'path': file_path, 'overwrite': 'true'}
+        response = requests.get(URL, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
 
 
-genius_int = 0
-for hero in superheroes_int:
-    for int_ in hero:
-        if hero[int_] > genius_int:
-            genius_int = hero[int_]
-            genius = int_
-print(f' Самый умный {genius}')
+    def upload_file_to_disk(self, file_path, filename):
+        response_href = self.upload(file_path)
+        href = response_href.get('href', '')
+        response = requests.put(href, data=open(filename, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print('Success')
+
+if __name__ == '__main__':
+
+    token = 'AQAAAAAO-mTuAADLW71PcQPokU32uhIYKYcMw10'
+
+    filename = 'romanchik.txt'
+    file_path = 'romanchik.txt'
+    ya = YaUploader(token)
+    ya.upload_file_to_disk(file_path=file_path,filename=filename)
